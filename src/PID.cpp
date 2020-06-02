70d89fa7ee1b152cd10c886ad923c4edd040df5e
Paul Yeh <pyeh.tw@icloud.com>
2020-06-02 10:27:49 +0200

    Init()

--- src/PID.cpp
+++ src/PID.cpp
@@ -8,23 +8,30 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp_, double Ki_, double Kd_) {
+void PID::Init(double Kp, double Ki, double Kd)
+{
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
-
+  Kp_ = Kp;
+  Ki_ = Ki;
+  Kd_ = Kd;
+  p_error_ = 0.0F;
+  i_error_ = 0.0F;
+  d_error_ = 0.0F;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte)
+{
   /**
    * TODO: Update PID errors based on cte.
    */
-
 }
 
-double PID::TotalError() {
+double PID::TotalError()
+{
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  return 0.0; // TODO: Add your total error calc here!
 }
--- src/PID.h
+++ src/PID.h
@@ -17,7 +17,7 @@ class PID {
    * Initialize PID.
    * @param (Kp_, Ki_, Kd_) The initial PID coefficients
    */
-  void Init(double Kp_, double Ki_, double Kd_);
+  void Init(double Kp, double Ki, double Kd);
 
   /**
    * Update the PID error variables given cross track error.
@@ -35,16 +35,16 @@ class PID {
   /**
    * PID Errors
    */
-  double p_error;
-  double i_error;
-  double d_error;
+  double p_error_;
+  double i_error_;
+  double d_error_;
 
   /**
    * PID Coefficients
-   */ 
-  double Kp;
-  double Ki;
-  double Kd;
+   */
+  double Kp_;
+  double Ki_;
+  double Kd_;
 };
 
 #endif  // PID_H
