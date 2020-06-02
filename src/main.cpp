e3f5221ba757dbe928ee47d03558b995beb49328
Paul Yeh <pyeh.tw@icloud.com>
2020-06-02 10:59:28 +0200

    complete pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main()
   /**
    * TODO: Initialize the pid variable.
    */
-  pid.Init(0.0F, 0.0F, 0.0F);
+  pid.Init(0.1F, 0.0F, 10.0F);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -71,6 +71,8 @@ int main()
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
 
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value
